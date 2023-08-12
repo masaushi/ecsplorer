96807cdbb5c4d764666114542f6b7f3b24abcf64
masaushi <masato.deushi@gmail.com>
2023-08-12 15:32:08 +0900

    add cli options

--- cmd/ecsplorer.go
+++ cmd/ecsplorer.go
@@ -2,15 +2,66 @@ package cmd
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"runtime/debug"
 
 	"github.com/masaushi/ecsplorer/internal/app"
 	"github.com/masaushi/ecsplorer/internal/handler"
 )
 
-func Execute(_ []string) {
+// Version is the version of `ecsplorer`, and injected at build time.
+var Version = ""
+
+// Execute executes a whole process of ecsplorer.
+func Execute(args []string) {
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	flags.Usage = usage(flags)
+	version := flags.Bool("version", false, "show the version of ecsplorer")
+	help := flags.Bool("help", false, "help for ecsplorer")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		os.Exit(1)
+	}
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "ecsplorer version: %s\n", getVersion())
+		os.Exit(0)
+	}
+	if *help {
+		flags.Usage()
+		os.Exit(0)
+	}
+
 	ctx := context.Background()
 	if err := app.Start(ctx, handler.ClusterListHandler); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func usage(flags *flag.FlagSet) func() {
+	return func() {
+		s := "ecsplorer is a tool designed for easy CLI operations with AWS ECS.\n\n" +
+			"Usage of ecsplorer:\n" +
+			"\tecsplorer [--flags]\n\n" +
+			"for more information, see: https://github.com/masaushi/ecsplorer\n\n" +
+			"flags:\n"
+		fmt.Fprint(os.Stderr, s)
+		flags.PrintDefaults()
+	}
+}
+
+func getVersion() string {
+	if Version != "" {
+		return Version
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok || info.Main.Version == "" {
+		return "unknown"
+	}
+
+	return info.Main.Version
 }
